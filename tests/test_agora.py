32bcceb26ff1922ec33e87c2a2b9ad7fbfc9e977
Jonas Renault <jonasrenault@gmail.com>
2026-09-11 11:53:28 +0200

    skip failing test in ci

--- tests/test_agora.py
+++ tests/test_agora.py
@@ -1,7 +1,13 @@
 import locale
+import os
 from datetime import date
 
+import pytest
+
 from agora.agora import SlotColor, _full_month_french, _get_slot_color
+
+# Check if running in GitHub Actions
+IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 
 
 def test_get_slot_color():
@@ -11,6 +17,10 @@ def test_get_slot_color():
     assert _get_slot_color("rgb(241, 114, 130") is SlotColor.red
 
 
+@pytest.mark.skipif(
+    IN_GITHUB_ACTIONS,
+    reason="Test does not work in GitHub Actions due to missing locale.",
+)
 def test_full_month_french():
     locale.setlocale(locale.LC_ALL, "fr_FR")
     for m in range(1, 13):
